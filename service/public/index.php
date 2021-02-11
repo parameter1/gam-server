b8db4410d0af856ec7f76a0eeaf60a86e085fffe
Jacob Bare <jacob@parameter1.com>
2021-02-11 11:07:40 -0600

    Remove unused classes

--- service/public/index.php
+++ service/public/index.php
@@ -3,15 +3,12 @@ require '../vendor/autoload.php';
 
 use Parameter1\Controller\DefaultController;
 use Parameter1\Error\JsonErrorRenderer;
-use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\AppFactory;
 use Slim\Routing\RouteCollectorProxy;
 
-use Slim\Exception\HttpNotFoundException;
-use Slim\Exception\HttpException;
-
 $app = AppFactory::create();
 
+// Enable body parsing and routing
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
 
